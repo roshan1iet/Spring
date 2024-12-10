59298cfc0fbe78699b1cff6547904d24bed69496
Roshan <sroshan27@gmail.com>
2024-12-10 16:56:58 +0530

    SpringBoot Form Validation

---
+++ FormValidation/src/main/java/com/example/FormValidation/FormValidationApplication.java
@@ -0,0 +1,15 @@
+package com.example.FormValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class FormValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormValidationApplication.class, args);
+		System.out.println("Hello World!!!");
+	}
+
+}
